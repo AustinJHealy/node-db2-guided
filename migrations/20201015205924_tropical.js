233c9ee23174e2c522e0ab0867de73b21441674f
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-10-15 21:25:07 -0400

    learned about knex migrations and seeds

---
+++ migrations/20201015205924_tropical.js
@@ -0,0 +1,14 @@
+const { table } = require("../data/config");
+
+exports.up = async function(knex) {
+  await knex.schema.alterTable("fruits", (table) => {
+      table.boolean("tropical").defaultTo(false)
+  })
+  
+};
+
+exports.down = async function(knex) {
+  await knex.schema.alterTable("fruits", (table) => {
+      table.dropColumn("tropical")
+  })
+};
